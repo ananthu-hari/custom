58278fff5b3cbd3e0cac64625b2094330e3df5dc
Ananthakrishnan T H <ananthakrishnan.th@gmail.com>
2023-07-01 15:37:16 +0530

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	deleted:    mymodule/mymodule.info.yml 	deleted:    mymodule/mymodule.links.menu.yml 	deleted:    mymodule/mymodule.module 	deleted:    mymodule/mymodule.routing.yml 	deleted:    mymodule/src/Controller/FirstController.php 	deleted:    mymodule/src/Controller/custom.php 	deleted:    rsvplist/rsvplist.info.yml 	deleted:    rsvplist/rsvplist.install 	deleted:    rsvplist/rsvplist.routing.yml 	deleted:    rsvplist/src/Form/RSVPForm.php

--- mymodule/src/Controller/custom.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function t($string, array $args = array(), array $options = array()) {
-  return \Drupal::translation()->translate($string, $args, $options);
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> Remote-floatsys/main
