6bb8fb0d4231b0c08bf50366fc7bd8c2e5c56dc8
snehasg03 <snehasibi03@gmail.com>
2023-07-02 19:07:45 +0530

    On branch main Changes to be committed: modified:   surveymanager/src/Form/JobCertificatesForm.php

--- surveymanager/src/Form/JobCertificatesForm.php
+++ surveymanager/src/Form/JobCertificatesForm.php
@@ -4,11 +4,39 @@ namespace Drupal\surveymanager\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Job Certificates form.
  */
 class JobCertificatesForm extends FormBase {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * JobCertificatesForm constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -22,14 +50,16 @@ class JobCertificatesForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state): array {
     $form['job_id'] = [
-      '#type' => 'number',
+      '#type' => 'entity_autocomplete',
       '#title' => $this->t('Job ID'),
+      '#target_type' => 'job_entity', // Replace 'job_entity' with the correct entity type for jobs.
       '#required' => TRUE,
     ];
 
     $form['certificate_id'] = [
-      '#type' => 'number',
+      '#type' => 'entity_autocomplete',
       '#title' => $this->t('Certificate ID'),
+      '#target_type' => 'certificate_entity', // Replace 'certificate_entity' with the correct entity type for certificates.
       '#required' => TRUE,
     ];
 
