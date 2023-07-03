2dadea353089e29f86d3a4d78d2207c57eb76710
pranav-priyesh <prapri16082002@gmail.com>
2023-07-03 11:32:56 +0530

    On branch main  Changes to be committed: 	modified:   surveymanager/js/main.js

--- surveymanager/js/main.js
+++ surveymanager/js/main.js
@@ -1,6 +1,9 @@
 var datatablesOptions = {
     "serverSide": true,
-    "ajaxSource": '[yourAjaxUrl]',
+    "ajax": {
+        "url": ajax.url,
+        "type": "POST" // or "GET" depending on your server-side implementation
+    },
     "processing": true,
     "columns": [
         null,
@@ -9,18 +12,16 @@ var datatablesOptions = {
         null,
         /* EDIT */ {
             mRender: function (data, type, row) {
-                return '<a class="table-edit" data-id="' + row[0] + '">EDIT</a>'
+                return '<a class="table-edit" data-id="' + row[0] + '">EDIT</a>';
             }
         },
         /* DELETE */ {
             mRender: function (data, type, row) {
-                return '<a class="table-delete" data-id="' + row[0] + '">DELETE</a>'
+                return '<a class="table-delete" data-id="' + row[0] + '">DELETE</a>';
             }
-        },              
-     ]
+        }
+    ]
 };
-
-
 
 $(document).ready(function () {
     $('#usertable').DataTable(datatablesOptions);
